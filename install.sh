c8ed3ca626f14dd62f703df14ccc5a221838ac79
Pietro Bertera <pietro@bertera.it>
2020-09-04 20:27:21 +0200

    vendor from ENV

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 VER="1.0.3"
-target_dir="${TARGET_DIR}"
+vendor=${VENDOR-bertera.it}
+target_dir="${TARGET_DIR-/etc/kubernetes/kubelet-plugins/volume/exec}"
+
 echo "begin to install smb FlexVolume driver ${VER} ..." 
 
-if [[ -z "${target_dir}" ]]; then
-  target_dir="/etc/kubernetes/kubelet-plugins/volume/exec"
-fi
+#if [[ -z "${target_dir}" ]]; then
+#  target_dir="/etc/kubernetes/kubelet-plugins/volume/exec"
+#fi
 
-smb_vol_dir="${target_dir}/microsoft.com~smb"
+smb_vol_dir="${target_dir}/${vendor}~smb"
 mkdir -p ${smb_vol_dir}
 
 if [ "$INSTALL_DEPS" = true ] ; then
