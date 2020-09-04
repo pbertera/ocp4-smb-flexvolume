132090a92092db659973a901756d289faa4c0854
Pietro Bertera <pietro@bertera.it>
2020-09-04 20:33:01 +0200

    MOD: error controls

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ VER="1.0.3"
 vendor=${VENDOR-bertera.it}
 target_dir="${TARGET_DIR-/etc/kubernetes/kubelet-plugins/volume/exec}"
 
+function bailout {
+    echo ERROR: $@
+    exit 1
+}
+
 echo "begin to install smb FlexVolume driver ${VER} ..." 
 
 #if [[ -z "${target_dir}" ]]; then
@@ -11,26 +16,22 @@ echo "begin to install smb FlexVolume driver ${VER} ..."
 #fi
 
 smb_vol_dir="${target_dir}/${vendor}~smb"
-mkdir -p ${smb_vol_dir}
+mkdir -p ${smb_vol_dir} || bailout creating plugin directory ${smb_vol_dir}
 
 if [ "$INSTALL_DEPS" = true ] ; then
   echo "installing statically linked dependencies (jq, cifs-utils)" 
   # copy any other static deps
-  cp ${SOURCE_DIR}/* ${smb_vol_dir}
+  cp ${SOURCE_DIR}/* ${smb_vol_dir} || bailout installing deps
 else
   echo "skipping installing deps: jq and cifs-utils must be pre-installed" 
 fi
 
 #copy smb script
-cp /bin/smb ${smb_vol_dir}/smb
-chmod a+x ${smb_vol_dir}/smb
+cp /bin/smb ${smb_vol_dir}/smb || bailout installing the plugin
+chmod a+x ${smb_vol_dir}/smb || bailout setting the permissions
 
 echo "install smb FlexVolume driver completed."
 
-#https://github.com/kubernetes/kubernetes/issues/17182
-# if we are running on kubernetes cluster as a daemon set we should
-# not exit otherwise, container will restart and goes into crashloop (even if exit code is 0)
-echo "install done, daemonset sleeping"
 while true; do 
     sleep 3600
 done
